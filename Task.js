d2efee65e8e531bd9be07b71a8358a7e501e4247
keiji2214 <o.keiji2214@gmail.com>
2020-08-14 11:20:21 +0900

    課題完了

--- Task.js
+++ Task.js
@@ -1,1 +1,9 @@
-test
+for(let i = 3; i < 100; i++){
+  if((i % 3 === 0)&&(i % 5 === 0)){
+    console.log("FizzBuzz",i);
+  }else if(i % 3 === 0){
+    console.log("Fizz",i);
+  }else if(i % 5 === 0){
+    console.log("Buzz",i);
+  }
+  }
